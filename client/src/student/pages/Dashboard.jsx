1cac91a5f1143f6bdc242467b6ca30a2bc2c1adb
Karan857 <kankhanthong@gmail.com>
2024-07-06 23:47:06 +0700

    test condition

--- client/src/student/pages/Dashboard.jsx
+++ client/src/student/pages/Dashboard.jsx
@@ -1,12 +1,15 @@
 // import React from 'react'
 import DashCountAc from '../components/DashCountAc'
 import List_Activity from '../components/List_Activity'
+import Test from '../components/Test'
 
 function Dashboard() {
   return (
     <div className='mt-10'>
-      <DashCountAc />
-      <List_Activity />
+      <Test/>
+      <hr className='m-[50px]'/>
+      {/* <DashCountAc />
+      <List_Activity /> */}
     </div>
   )
 }
