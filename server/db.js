e6e21fdb35c3af4880138998ec3161fa00ed1517
Karan857 <kankhanthong@gmail.com>
2024-08-22 19:26:45 +0700

    fix12

--- server/db.js
+++ server/db.js
@@ -1,10 +1,14 @@
 import mysql from 'mysql2'
 
 const db = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "activitydb"
+    // host: "localhost",
+    // user: "root",
+    // password: "",
+    // database: "activitydb"
+    host: process.env.DB_HOST,
+    user: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DBNAME,
 })
 export default db
 
