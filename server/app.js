f8d542b4023d21ec5d4e9fa28d1b383b97cce119
Karan857 <kankhanthong@gmail.com>
2024-08-22 19:00:00 +0700

    fix09

--- server/app.js
+++ server/app.js
@@ -37,7 +37,8 @@ app.use('/status', status)
 app.use('/', notify)
 
 
-app.listen(3000, () => console.log('server on port 3000'))
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log('server on port 3000'))
 
 
 
