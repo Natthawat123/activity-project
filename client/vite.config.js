ec613b44fb652141d1f2518e6f65a4fd922446a0
Karan857 <kankhanthong@gmail.com>
2024-08-23 19:09:52 +0700

    f4

--- client/vite.config.js
+++ client/vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "https://activity-project-l9oq.onrender.com/",
+        target: "https://activity-project-l9oq.onrender.com",
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ""),
       },
