d5de22f88e4e4be7db5047cc3b5f71401f64bd25
Karan857 <kankhanthong@gmail.com>
2024-08-23 17:55:27 +0700

    delete semver

--- server/db.js
+++ server/db.js
@@ -4,7 +4,7 @@ const db = mysql.createConnection({
     // host: "localhost",
     // user: "root",
     // password: "",
-    // database: "activitydb"
+    // database: "activitydb"dddd
     host: process.env.DB_HOST,
     user: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
