7b9a0683a371a027832d1dacf59183433b60b367
Burinchai Suk-on <demonkung55@gmail.com>
2024-08-27 08:33:04 +0000

    Update file vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,20 +1,15 @@
-import {
-  defineConfig
-} from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      "/api": {
-        target: "https://activity-project-mioo.onrender.com",
+      '/api': {
+        target: 'https://activity-project-mioo.onrender.com',
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ""),
+        rewrite: (path) => path.replace(/^\/api/, ''),
       },
     },
-    
   },
-
-})
+});
