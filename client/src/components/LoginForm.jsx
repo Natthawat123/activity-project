3e71777c244f9feba623ca4b8264f1a25f762b8d
Burinchai Suk-on <demonkung55@gmail.com>
2024-08-29 02:44:36 +0000

    Update file LoginForm.jsx

--- client/src/components/LoginForm.jsx
+++ client/src/components/LoginForm.jsx
@@ -24,6 +24,7 @@ export default function SignInSide() {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
+          "Accept": "application/json",
         },
         body: JSON.stringify(jsonData),
       });
