25722e75d8af8943329302e8a72e0ff1a8968e97
Burinchai Suk-on <demonkung55@gmail.com>
2024-08-29 02:50:46 +0000

    Update file LoginForm.jsx

--- client/src/components/LoginForm.jsx
+++ client/src/components/LoginForm.jsx
@@ -15,8 +15,8 @@ export default function SignInSide() {
     event.preventDefault();
     const data = new FormData(event.currentTarget);
     const jsonData = {
-      username: data.get("username"),
-      password: data.get("password"),
+      username: credentials && credentials.data.get("username"),
+      password: credentials && credentials.data.get("password"),
     };
 
     try {
